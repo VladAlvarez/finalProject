0470c570417e961d38731648cd899bd1ed1c9697
Vlad Alvarez <moroni.v.alvarez@gmail.com>
2023-10-29 19:41:24 -0600

    mostly working

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
 // array to store comments
-let commentSection = [
-    {
-        name: "Hello",
-        comment: "Hello World"
-    }
-]
+// let commentSection = [
+//     {
+//         name: "Hello",
+//         comment: "Hello World"
+//     }
+// ]
 
 // add comment to array
-$("#submit").click( function () {
-    commentSection.push("poop");
-});
+// $("#submit").click( function () {
+//     commentSection.push("poop");
+// });
 
 // Going through array and appending them to index.html
-for (let i = 0; i < commentSection.length; i++) {
-      $("#container").append('<div class="commentContainer">'
-        + '<div class="displayName">' + commentSection[i].name + "</div>"
-        + '<div class="comment">' + commentSection[i].comment + "</div>"
-        + '</div>');
-  };
+// for (let i = 0; i < commentSection.length; i++) {
+//       $("#container").append('<div class="commentContainer">'
+//         + '<div class="displayName">' + commentSection[i].name + "</div>"
+//         + '<div class="comment">' + commentSection[i].comment + "</div>"
+//         + '</div>');
+//   };
 
 
+// add comment function
+$("#submit").click(function () {
+        const comment = document.getElementById("commentWriter").value
+        const displayName = document.getElementById("displayNameWriter").value
+        const containerChildrenCount = document.getElementById("container").childElementCount;
+        $("#container").append(
+        `<div id="entry${containerChildrenCount}" class="entry">
+            <span class="material-symbols-outlined">account_circle</span>
+            <div id="displayName${containerChildrenCount}" class="displayName">${displayName}</div>
+            <div id="comment${containerChildrenCount}" class="comment">${comment}</div>
+            <div id="edit${containerChildrenCount}">edit</div>
+            <div id="delete${containerChildrenCount}">delete</div>
+            <div id="editCommentContainer${containerChildrenCount}" class="hidden">
+                <input type="text" placeholder="${comment}">
+                <div id="newSubmit${containerChildrenCount}">Submit</div>
+            </div>
+            </div>`
+            );
+        $(`#edit${containerChildrenCount}`).click(function () {
+            $(`#editCommentContainer${containerChildrenCount}`).toggleClass("displayEdit");
+        });
+        $(`#delete${containerChildrenCount}`).click(function () {
+            $(`#entry${containerChildrenCount}`).remove()
+        });
+});
