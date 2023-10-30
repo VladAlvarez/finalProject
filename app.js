2c0a8d497b3a0714b7444b1c157214e2181778e5
Vlad Alvarez <moroni.v.alvarez@gmail.com>
2023-10-29 19:59:48 -0600

    clean up comments

--- app.js
+++ app.js
@@ -1,26 +1,4 @@
-// array to store comments
-// let commentSection = [
-//     {
-//         name: "Hello",
-//         comment: "Hello World"
-//     }
-// ]
-
-// add comment to array
-// $("#submit").click( function () {
-//     commentSection.push("poop");
-// });
-
-// Going through array and appending them to index.html
-// for (let i = 0; i < commentSection.length; i++) {
-//       $("#container").append('<div class="commentContainer">'
-//         + '<div class="displayName">' + commentSection[i].name + "</div>"
-//         + '<div class="comment">' + commentSection[i].comment + "</div>"
-//         + '</div>');
-//   };
-
-
-// add comment function
+// add entry
 $("#submit").click(function () {
         const comment = document.getElementById("commentWriter").value
         const displayName = document.getElementById("displayNameWriter").value
