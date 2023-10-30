459d5c907e15afb6ba4240b0fd007ca967a40189
Vlad Alvarez <moroni.v.alvarez@gmail.com>
2023-10-30 00:56:28 -0600

    added all styles for entries

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ $("#submit").click(function () {
         const containerChildrenCount = document.getElementById("container").childElementCount;
         $("#container").append(
         `<div id="entry${containerChildrenCount}" class="entry">
-            <span class="material-symbols-outlined">account_circle</span>
-            <div id="displayName${containerChildrenCount}" class="displayName">${displayName}</div>
-            <div id="comment${containerChildrenCount}" class="comment">${comment}</div>
-            <div id="edit${containerChildrenCount}">edit</div>
-            <div id="delete${containerChildrenCount}">delete</div>
-            <div id="editCommentContainer${containerChildrenCount}" class="hidden">
-                <input type="text" id="newInput${containerChildrenCount}" placeholder="${comment}">
-                <div id="newSubmit${containerChildrenCount}">Submit</div>
+            <span class="material-symbols-outlined" >account_circle</span>
+            <div class="fullContainer">
+                <div class="topContainer">
+                    <div id="displayName${containerChildrenCount}" class="displayName">${displayName}</div>
+                    <div class="editDelete">
+                        <div id="edit${containerChildrenCount}" class="edit">edit</div>
+                        <div id="delete${containerChildrenCount}" class="delete">delete</div>
+                    </div>
+                </div>
+                <div id="comment${containerChildrenCount}" class="comment">${comment}</div>
+                <div id="editCommentContainer${containerChildrenCount}" class="hidden">
+                    <input type="text" id="newInput${containerChildrenCount}" class="newInput" value="${comment}">
+                    <div id="newSubmit${containerChildrenCount}" class="newSubmit">Submit</div>
+                </div>
             </div>
-            </div>`
+        </div>`
             );
         $(`#edit${containerChildrenCount}`).click(function () {
             $(`#editCommentContainer${containerChildrenCount}`).toggleClass("displayEdit");
